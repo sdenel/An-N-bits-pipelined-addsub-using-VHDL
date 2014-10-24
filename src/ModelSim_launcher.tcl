f58bcb334301e608c8b5eadd17bf1a253ccc24b4
Simon DENEL <simondenel1@gmail.com>
2014-10-24 14:32:43 +0200

    Update ModelSim_launcher.tcl

--- src/ModelSim_launcher.tcl
+++ src/ModelSim_launcher.tcl
@@ -10,15 +10,15 @@ add wave -position end  -radix decimal sim:/tb_addsub_rtl/a
 add wave -position end  -radix binary sim:/tb_addsub_rtl/b
 add wave -position end  -radix decimal sim:/tb_addsub_rtl/b
 
-add wave -divider {Additionneur/Diviseur avec 0 registre de pipeline}
+add wave -divider {AddSub with 0 pipeline registers}
 add wave -position end  -radix binary sim:/tb_addsub_rtl/z0
 add wave -position end  -radix decimal sim:/tb_addsub_rtl/z0
 
-add wave -divider {Additionneur/Diviseur avec 1 registres de pipeline}
+add wave -divider {AddSub with 1 pipeline registers}
 add wave -position end  -radix binary sim:/tb_addsub_rtl/z1
 add wave -position end  -radix decimal sim:/tb_addsub_rtl/z1
 
-add wave -divider {Additionneur/Diviseur avec 1+2=3 registres de pipeline}
+add wave -divider {AddSub with 1+2=3 pipeline registers}
 add wave -position end  -radix binary sim:/tb_addsub_rtl/z2
 add wave -position end  -radix decimal sim:/tb_addsub_rtl/z2
 run 500ns
